b8527ee3d649df173c4e8e180422d66577113b75
net9keep <net9keep@naver.com>
2017-08-03 14:27:56 +0900

    smtp final

--- blog/views.py
+++ blog/views.py
@@ -76,7 +76,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post' : post})
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     send_mail()
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request,'blog/post_list.html',{'posts' : posts})
 
