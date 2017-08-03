0a99b77c615010ee577ad240f2cff45666bee68a
net9keep <net9keep@naver.com>
2017-08-03 14:08:43 +0900

    smtp test

--- blog/views.py
+++ blog/views.py
@@ -15,33 +15,32 @@ from .models import Post
 from .forms import PostForm
 # Create your views here.
 
-cc_user = ["tae0code@gmail.com","net9keep@naver.com"]
-smtp_server  = "smtp.naver.com"
-port = 465
-userid = "net9keep"
-passwd = "qs13e2rd13w2a"
 
 
-def send_mail(from_user, to_user, subject, text, attach):
-    COMMASPACE = ", "
-    msg = MIMEMultipart("alternative")
-    msg["From"] = from_user
-    msg["To"] = to_user
-    msg["Subject"] = Header(s=subject, charset="utf-8")
-    msg["Date"] = Utils.formatdate(localtime=1)
-    msg.attach(MIMEText(text, "html", _charset="utf-8"))
 
-    if (attach != None):
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(open(attach, "rb").read())
-        Encoders.encode_base64(part)
-        part.add_header("Content-Disposition", "attachment; filename=\"%s\"" % os.path.basename(attach))
-        msg.attach(part)
 
-    smtp = smtplib.SMTP(smtp_server, port)
-    smtp.login(userid, passwd)
-    smtp.sendmail(from_user, msg.as_string())
-    smtp.close()
+def send_mail():
+    senderAddr = "net9keep@naver.com"
+
+    recipientAddr = "tae0code@gmail.com"
+
+    text = "smpt test"
+
+    msg = MIMEText(text, _charset="utf8")
+
+    msg['Subject'] = "smtp test"
+
+    msg['From'] = senderAddr
+
+    msg['to'] = recipientAddr
+
+    s = smtplib.SMTP_SSL('smtp.naver.com.com', 465)
+
+    s.login(senderAddr, "qs13e2rd13w2a")
+
+    s.sendmail(senderAddr, recipientAddr, msg.as_string())
+
+    s.quit()
 
 
 def post_edit(request, pk):
@@ -66,7 +65,7 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            send_mail("net9keep@naver.com", "tae0code@gmail.com", "test", "test")
+            send_mail()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
@@ -78,6 +77,6 @@ def post_detail(request, pk):
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    send_mail("net9keep@naver.com", "tae0code@gmail.com", "test", "test")
+    send_mail()
     return render(request,'blog/post_list.html',{'posts' : posts})
 
